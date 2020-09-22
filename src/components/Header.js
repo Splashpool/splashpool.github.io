4d2d374dcdae1cbd80ad39f9f46f1228de558390
Eva Coffield <eva@darcoff.com>
2020-09-22 20:23:52 +0100

    Added new Header component

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import './Header.css';
+
+function Header() {
+    return (
+        <div className="Header">
+            <Typography variant="h1" component="h2" gutterBottom className="custom-font">
+                Welcome to Oasis
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+                Find for your nearest drinking water and toilet facilities
+            </Typography>
+            <Button variant="contained" color="primary">Search locations
+            </Button>
+        </div>
+    );
+}
+
+export default Header;
