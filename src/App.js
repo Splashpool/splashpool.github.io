0ab208175d114f9f69ea92efcf8b0ed06e0be357
sanae <sanae.k1986@gmail.com>
2020-09-24 20:47:54 +0100

    first try in react for sanae , add state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
+import LocationListing from './components/LocationListing';
 import "fontsource-montserrat";
 
+
 function App() {
+
+  const [locations,setlocation]=useState([
+    {name:"london",longitude:"51.5074",latitude:" 0.1278",status:"Drinkable"},
+    {name:"Santiago",longitude:"54.5074",latitude:" 7.1278",status:"Need traitment"},
+    {name:"Santiago",longitude:"94.5074",latitude:" 7.1278",status:"Drinkable"},
+    {name:"Santiago",longitude:"04.5074",latitude:" 7.1278",status:"Uknown"}
+
+  ]);
+
   return (
     <div className="App">
       <Header />
+      <LocationListing locationData={locations} />
     </div>
   );
 }
