4c5c3bfba3a60c7cfe6c48b459d41620b022d362
Eva Coffield <eva@darcoff.com>
2020-09-25 22:07:48 +0100

    Implemented Basic Routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Header from './components/Header';
 import LocationListing from './components/LocationListing';
 import "fontsource-montserrat";
 
 
 function App() {
-
   const [locations, setlocation] = useState([
     { name: "london", longitude: "51.5074", latitude: " 0.1278", status: "Drinkable" },
     { name: "Santiago", longitude: "54.5074", latitude: " 7.1278", status: "Need traitment" },
@@ -16,11 +16,54 @@ function App() {
   ]);
 
   return (
-    <div className="App">
-      <Header />
-      <LocationListing locationData={locations} />
-    </div>
+    <Router>
+      <div className="App">
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        </div>
+        <Header />
+        <LocationListing locationData={locations} />
+      </div>
+    </Router>
   );
 }
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+
 
 export default App;
