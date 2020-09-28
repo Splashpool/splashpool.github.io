60f577a2da9429a1045e9b709b6b92fa17b7ec67
Elena Nikolova <elena.nikolova190@gmail.com>
2020-09-28 21:45:07 +0100

    With filter switches inactive

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Header from './components/Header';
-import LocationListing from './components/LocationListing';
+import Header from "./components/Header";
+import LocationListing from "./components/LocationListing";
 import "fontsource-montserrat";
-
+import Filter from "./components/Filter";
 
 function App() {
   const [locations, setlocation] = useState([
-    { name: "london", longitude: "51.5074", latitude: " 0.1278", status: "Drinkable" },
-    { name: "Santiago", longitude: "54.5074", latitude: " 7.1278", status: "Need traitment" },
-    { name: "Santiago", longitude: "94.5074", latitude: " 7.1278", status: "Drinkable" },
-    { name: "Santiago", longitude: "04.5074", latitude: " 7.1278", status: "Uknown" }
-
+    {
+      name: "london",
+      longitude: "51.5074",
+      latitude: " 0.1278",
+      status: "Drinkable",
+    },
+    {
+      name: "Santiago",
+      longitude: "54.5074",
+      latitude: " 7.1278",
+      status: "Need traitment",
+    },
+    {
+      name: "Santiago",
+      longitude: "94.5074",
+      latitude: " 7.1278",
+      status: "Drinkable",
+    },
+    {
+      name: "Santiago",
+      longitude: "04.5074",
+      latitude: " 7.1278",
+      status: "Uknown",
+    },
   ]);
 
   return (
@@ -33,20 +52,30 @@ function App() {
             </ul>
           </nav>
 
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
         </div>
         <Header />
         <LocationListing locationData={locations} />
+        <div class="mui-row">
+          <div class="mui-col-md-8 mui-col-md-offset-2">
+            <img src="../public/placeholder.png" class="image" />
+          </div>
+        </div>
+        <div class="mui-row">
+          <div class="mui-col-md-8 mui-col-md-offset-2">
+            <Filter />
+          </div>
+        </div>
       </div>
     </Router>
   );
@@ -63,7 +92,5 @@ function About() {
 function Users() {
   return <h2>Users</h2>;
 }
-
-
 
 export default App;
