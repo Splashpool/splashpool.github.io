7035d57d3fb1f97af158e012299a651274b1082e
Eva Coffield <eva@darcoff.com>
2020-10-05 23:00:55 +0100

    Setting up login form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Component } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Grid from '@material-ui/core/Grid';
@@ -8,135 +8,127 @@ import Link from "@material-ui/core/Link";
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import TextField from "@material-ui/core/TextField";
-import { makeStyles } from '@material-ui/core/styles';
+//import { makeStyles } from '@material-ui/core/styles';
 import './Login.css';
 
+class Login extends Component {
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
 
-function Login() {
+  // const useStyles = makeStyles((theme) => ({
+  //   paper: {
+  //     marginTop: theme.spacing(8),
+  //     display: 'flex',
+  //     flexDirection: 'column',
+  //     alignItems: 'center',
+  //   },
+  //   avatar: {
+  //     margin: theme.spacing(1),
+  //     backgroundColor: theme.palette.secondary.main,
+  //   },
+  //   form: {
+  //     width: '100%', // Fix IE 11 issue.
+  //     marginTop: theme.spacing(1),
+  //   },
+  //   submit: {
+  //     margin: theme.spacing(3, 0, 2),
+  //   },
+  // }));
 
-  const classes = useStyles();
-  const addEmail = (email) => {
-    console.log(email);
+  // const classes = useStyles();
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+  
+  login() {
+    console.log('this.state', this.state);
   }
 
-  // const EmailInput = () => {
-  //     const [email] = useState("");
-  //     console.log("Email changed");
-  // } 
-
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-    console.log("Set email");
-  }
-
-  const setEmail = (email) => {
-    console.log(email);
-  }
-
-  // const handleAddClick = () => {
-  //         addEmail(email);
-  //         console.log(email);
-  // }
-
-  return (
-    <div className="Login">
-      <Grid container spacing={0}>
-      <Grid item md></Grid>
-        <Grid item xs={12} md={6}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-          <Typography
-            variant="h3"
-            component="h1"
-            gutterBottom
-          >
-            Login with Email
+  state = {}
+  render() {
+    return (
+      <div className="Login">
+        <Grid container spacing={0}>
+          <Grid item md></Grid>
+          <Grid item xs={12} md={6}>
+            <Avatar /*className={classes.avatar}*/>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography
+              variant="h3"
+              component="h1"
+              gutterBottom
+            >
+              Login with Email
       </Typography>
-          <Typography variant="body1" gutterBottom>
-            Enter the email address associated with your account
+            <Typography variant="body1" gutterBottom>
+              Enter the email address associated with your account
       </Typography>
-          <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-            addEmailFunction={addEmail}
-            // value={ email } 
-            onChange={handleEmailChange}
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-            /*onClick={ handleAddClick }*/
-          >
-            Continue
+            <form /*className={classes.form}*/ noValidate>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                onChange={event => this.setState({ email: event.target.value })}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={event => this.setState({ password: event.target.value })}
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                /*className={classes.submit}*/
+                onClick={() => this.login()}
+              >
+                Continue
           </Button>
-          </form>
+            </form>
 
 
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body1">
-                Forgot password?
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body1">
+                  Forgot password?
               </Link>
-            </Grid>
-            <Grid item>No account.
+              </Grid>
+              <Grid item>No account.
               <Link href="#" variant="body1" gutterBottom>
-                {" Create one"}
-              </Link>
+                  {" Create one"}
+                </Link>
+              </Grid>
             </Grid>
-          </Grid>
-          
-        </Grid>
-        <Grid item md></Grid>
-      </Grid>
-    </div>
-  );
-}
 
+          </Grid>
+          <Grid item md></Grid>
+        </Grid>
+      </div>
+
+    );
+  }
+}
 export default Login;
