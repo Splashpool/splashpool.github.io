d9a6bb675d5d9e969f90c21c413b01da291f09ac
Eva Coffield <eva@darcoff.com>
2020-10-13 10:53:29 +0100

    Innitially set up Drawer component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,46 @@
-import React, { useState } from "react";
-import { StylesProvider } from "@material-ui/core/styles";
+import React from 'react';
+import { Drawer, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import { makeStyles, StylesProvider } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useRouteMatch,
-  useParams,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./components/Home/Home";
-import Login from "./components/Login/Login.jsx";
-import Menu from "./components/Menu/Menu";
-import LocationListing from "./components/LocationListing";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
 import LocationDetails from "./components/LocationDetails";
 import "fontsource-montserrat";
 import Search from "./components/Search/Search.jsx";
 import Filter from "./components/Filter";
-import MapView from "./components/MapView";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { faHandHoldingWater } from "@fortawesome/free-solid-svg-icons";
 import Copyright from "./components/Copyright/Copyright";
+import HomeIcon from '@material-ui/icons/Home';
+import MyLocationIcon from '@material-ui/icons/MyLocation';
+import RoomIcon from '@material-ui/icons/Room';
+import SearchIcon from '@material-ui/icons/Search';
+import LockIcon from '@material-ui/icons/Lock';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import Header from './components/Header/Header';
+
 
 library.add(fab, faHandHoldingWater);
 
+const useStyles = makeStyles((theme) => ({
+  drawerPaper: { width: 'inherit' },
+  link: {
+    textDecoration: 'none',
+    color: theme.palette.text.primary
+  },
+  gutterBottom: {
+    marginBottom: '1.5rem'
+  }
+}))
+
 function App() {
+
+  const classes = useStyles();
+
   // const [locations, setlocation] = useState([
   //   {
   //     id: 1,
@@ -61,46 +75,83 @@ function App() {
   return (
     <StylesProvider injectFirst>
       <Router>
-        <div className="App">
-          <Menu />
-          <Container maxWidth="md">
-            <div>
-              <nav>
-                <ul>
-                  <li>
-                    <Link to="/">Home</Link>
-                  </li>
-                  <li>
-                    <Link to="/locations">Locations</Link>
-                  </li>
-                  <li>
-                    <Link to="/search">Search</Link>
-                  </li>
-                  <li>
-                    <Link to="/login">Login</Link>
-                  </li>
-                  <li>
-                    <Link to="/location-details/:number">location-details</Link>
-                  </li>
-                </ul>
-              </nav>
-
-              <Switch>
-                <Route path="/locations">
-                  <Filter />
-                </Route>
-                <Route path="/search" component={Search} />
-                <Route path="/login" component={Login} />
-                <Route
-                  path="/location-details/:number"
-                  component={LocationDetails}
-                />
-                <Route path="/" exact={true} component={Home} />
-              </Switch>
-            </div>
-          </Container>
-          <Copyright />
+        <div style={{ display: 'flex' }}>
+          <Drawer
+            style={{ width: '240px' }}
+            varian="persistent"
+            anchor="left"
+            open={true}
+            classes={{ paper: classes.drawerPaper }}
+          >
+            <List>
+              <Link to="/" className={classes.link}>
+                <ListItem button>
+                  <ListItemIcon>
+                    <HomeIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={"Home"} />
+                </ListItem>
+              </Link>
+              <Link to="/locations" className={classes.link}>
+                <ListItem button>
+                  <ListItemIcon>
+                    <MyLocationIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={"Locations"} />
+                </ListItem>
+              </Link>
+              <Link to="/location-details/:number" className={classes.link}>
+                <ListItem button>
+                  <ListItemIcon>
+                    <RoomIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={"Locations Details"} />
+                </ListItem>
+              </Link>
+              <Link to="/search" className={classes.link}>
+                <ListItem button>
+                  <ListItemIcon>
+                    <SearchIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={"Search"} />
+                </ListItem>
+              </Link>
+              <Link to="/login" className={classes.link}>
+                <ListItem button>
+                  <ListItemIcon>
+                    <LockIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={"Login"} />
+                </ListItem>
+              </Link>
+              <Link to="/register" className={classes.link}>
+                <ListItem button>
+                  <ListItemIcon>
+                    <ExitToAppIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={"Register"} />
+                </ListItem>
+              </Link>
+            </List>
+          </Drawer>
         </div>
+        <Header />
+        <Container maxWidth="md">
+          <Switch>
+            <Route path="/locations">
+              <Filter />
+            </Route>
+            <Route path="/search" component={Search} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+            <Route
+              path="/location-details/:number"
+              component={LocationDetails}
+            />
+            <Route path="/" exact={true} component={Home} />
+          </Switch>
+        </Container>
+        <Copyright />
       </Router>
     </StylesProvider>
   );
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,13 +1,99 @@
 import React, { Component } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Grid from '@material-ui/core/Grid';
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Link from "@material-ui/core/Link";
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import TextField from "@material-ui/core/TextField";
 
 class Register extends Component {
     constructor () {
-        
+        super();
+        this.state = {
+            email: '',
+            password: ''
+          }
     }
     state = {  }
     render() {
         return (
-            
+            <Grid container spacing={0}>
+          <Grid item md></Grid>
+          <Grid item xs={12} md={6}>
+            <Avatar /*className={classes.avatar}*/>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography
+              variant="h3"
+              component="h1"
+              gutterBottom
+            >
+              Register
+      </Typography>
+            <Typography variant="body1" gutterBottom>
+              Enter the email address associated with your account
+      </Typography>
+            <form /*className={classes.form}*/ noValidate>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                onChange={event => this.setState({ email: event.target.value })}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={event => this.setState({ password: event.target.value })}
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                /*className={classes.submit}*/
+                onClick={() => this.login()}
+              >
+                Continue
+          </Button>
+            </form>
+
+
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body1">
+                  Forgot password?
+              </Link>
+              </Grid>
+              <Grid item>No account.
+              <Link href="#" variant="body1" gutterBottom>
+                  {" Create one"}
+                </Link>
+              </Grid>
+            </Grid>
+
+          </Grid>
+          <Grid item md></Grid>
+        </Grid>
         );
     }
 }
