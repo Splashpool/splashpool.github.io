255968a42b3cca64c39b6a118687d0a20e29f89e
sanae <sanae.k1986@gmail.com>
2020-10-29 11:36:28 +0000

    delete none used

--- src/components/LocationDetails.js
+++ src/components/LocationDetails.js
@@ -8,16 +8,11 @@ import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
+
 //import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import FetchLocation from './FetchLocation';
-import { useDispatch, useSelector } from 'react-redux';
-import { getLocation } from '../store/location/locationAction';
-import { locationList, getLocationLoading} from '../store/location/locationSelector';
+
 
 
 
