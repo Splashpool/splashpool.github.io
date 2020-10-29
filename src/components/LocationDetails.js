581bfe292ed37d594513dbd1a76668f52ce45d57
sanae <sanae.k1986@gmail.com>
2020-10-29 15:33:28 +0000

    get location detail data

--- src/components/LocationDetails.js
+++ src/components/LocationDetails.js
@@ -1,5 +1,6 @@
 //import React, { Component } from 'react';
-import React from 'react';
+import React, { useEffect, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Grid from '@material-ui/core/Grid';
 import Rating from 'material-ui-rating';
 import Typography from '@material-ui/core/Typography';
@@ -11,7 +12,9 @@ import FormControl from '@material-ui/core/FormControl';
 
 //import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
-import FetchLocation from './FetchLocation';
+import { getLocation } from '../store/location/locationAction';
+import { locationList } from '../store/location/locationSelector';
+
 
 
 
@@ -23,31 +26,35 @@ import FetchLocation from './FetchLocation';
 
 
 const LocationDetails = (features) => {
-    
-//     let d=  useEffect(() => {
 
-//         
+    //     let d=  useEffect(() => {
 
-//     }, [features.match.params.number]);
-//  console.log(d);
-// const dispatch = useDispatch();
-// const locationListSelector = useSelector(locationList);
-// const getLocationList = useCallback( () => {
-//      dispatch(getLocation());
-// }, [dispatch]);
-// useEffect( () => {
-//      getLocationList();
-// }, [getLocationList]);
+    //    
+    let locationId = features.match.params.number;
+    console.log('idddd', locationId);
+    const dispatch = useDispatch();
+    const locationListSelector = useSelector(locationList);
+    let locationData;
+
+    const getLocationList = useCallback(() => {
+        dispatch(getLocation());
+    }, [dispatch]);
+    useEffect(() => {
+        getLocationList();
+    }, [getLocationList]);
+    if (locationListSelector) {
+        locationData = locationListSelector.filter((location) => location.locationId === parseInt(locationId))[0];
+    }
+    console.log('datttta', locationData);
+
+    // let result=FetchLocation({ LocationId: parseInt(features.match.params.number)});
+    // var data = { id: 1, name: 'name', description: 'descriptionn', status: 'drinkable', stars: 4 };
+
+    // console.log(result);
+    // const result = locationListSelector.filter( loc=>( loc.locationId== parseInt(features.match.params.number))
+    //    )
 
 
-let result=FetchLocation({ LocationId: parseInt(features.match.params.number)});
-var data = { id: 1, name: 'name', description: 'descriptionn', status: 'drinkable', stars: 4 };
-
-console.log(result);
-// const result = locationListSelector.filter( loc=>( loc.locationId== parseInt(features.match.params.number))
-//    )
-
-   
 
     // }
     // const [value, setValue] = React.useState('drinkable');
@@ -65,64 +72,61 @@ console.log(result);
 
         search.removeChild(search.childNodes[0]);
     }
+    if (locationData) {
+        console.log('data  ', locationData);
+        let drinkable = '';
+        if (locationData.drinkable === true) {
+            drinkable = 'drinkable';
+        } else {
+            drinkable = 'treatment';
+        }
+        return (
 
-    return (
-
-        <Grid container spacing={0}>
-            <Grid item xs={12} sm={6}>
-            {/* {locationListSelector.map(location => (
-   
-   <li key={location.locationId}>{location.locationName}</li>
-
-   ))} */}
-                {/* to get this line working you need to install -> npm install material-ui-rating
-                 and  npm install @material-ui/icons */}
+            <Grid container spacing={0}>
+                <Grid item xs={12} sm={6}>
 
 
-                {/* <Card className="spl-root">
-                    <CardActionArea> */}
+                    <img
+                        className="spl-media"
+                        src="https://images.freeimages.com/images/large-previews/113/water-from-spring-1426253.jpg"
+                        alt="Contemplative Reptile"
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    {/* <CardContent> */}
+                    <Typography variant="h5">{locationData.locationName}</Typography>
 
-                <img
-                    className="spl-media"
-                    src="https://images.freeimages.com/images/large-previews/113/water-from-spring-1426253.jpg"
-                    alt="Contemplative Reptile"
-                />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-                {/* <CardContent> */}
-                <Typography variant="h5">Rating</Typography>
+                    <Rating
+                        value={4}
+                        max={5}
+                        onChange={(value) => console.log(`Rated with value ${value}`)}
+                    />
+                    <FormControl component="fieldset">
 
-                <Rating
-                    value={data.stars}
-                    max={5}
-                    onChange={(value) => console.log(`Rated with value ${value}`)}
-                />
-                <FormControl component="fieldset">
+                        <RadioGroup aria-label="gender" name="water" value={drinkable} onChange={handleChange} className="spl-radio">
 
-                    <RadioGroup aria-label="gender" name="water" value={data.status} onChange={handleChange} className="spl-radio">
-
-                        <FormControlLabel value="drinkable" control={<Radio color="primary" />} label="Drinkable" />
-                        <Divider />
-                        <FormControlLabel value="treatment" control={<Radio color="primary" />} label="Need treatment" />
-                        <Divider />
-                        <FormControlLabel value="undrinkable" control={<Radio color="primary" />} label="Undrinkable" />
-                        <Divider />
-                        <FormControlLabel value="unkown" control={<Radio color="primary" />} label="Unkown" />
-                    </RadioGroup>
-                </FormControl>
-            </Grid>
-            {/* </CardContent> */}
-            {/* </CardActionArea> */}
-            {/* <CardActions> */}
-            <Grid item xs={12}>
-                <Button size="small" color="primary">
-                    Report a problem
+                            <FormControlLabel value="drinkable" control={<Radio color="primary" />} label="Drinkable" />
+                            <Divider />
+                            <FormControlLabel value="treatment" control={<Radio color="primary" />} label="Need treatment" />
+                            <Divider />
+                            <FormControlLabel value="undrinkable" control={<Radio color="primary" />} label="Undrinkable" />
+                            <Divider />
+                            <FormControlLabel value="unkown" control={<Radio color="primary" />} label="Unkown" />
+                        </RadioGroup>
+                    </FormControl>
+                </Grid>
+                {/* </CardContent> */}
+                {/* </CardActionArea> */}
+                {/* <CardActions> */}
+                <Grid item xs={12}>
+                    <Button size="small" color="primary">
+                        Report a problem
                         </Button>
-                <Button size="small" variant="contained" color="primary" disableElevation>
-                    Notify me
+                    <Button size="small" variant="contained" color="primary" disableElevation>
+                        Notify me
                         </Button>
-            </Grid>
-            {/* </CardActions>
+                </Grid>
+                {/* </CardActions>
                 </Card>
  */}
 
@@ -130,9 +134,20 @@ console.log(result);
 
 
 
-        </Grid>
+            </Grid>
 
-    );
+        )
+    } else {
+        return (
+
+            <Grid container spacing={0}>
+                <Grid item xs={12} sm={6}>
+                    <Typography variant="h5">loading .....</Typography>
+
+                </Grid>
+            </Grid>
+        );
+    }
 
 }
 
